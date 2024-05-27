8f7260c5325ce86193c58c68c42da86a3b1e7023
Marvel School <9015646@student.zadkine.nl>
2024-05-27 09:50:37 +0200

    bruh

---
+++ wp-content/themes/NIEK HENDRIX/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        Hallo mijn naam is Niek Hendrix
+    </h1>
+
+    <?php wp_footer() ?>
+</body>
+</html>
