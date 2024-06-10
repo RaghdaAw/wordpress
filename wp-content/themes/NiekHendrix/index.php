c2065031b4c017e0dd1796b6df6fdb57ee5c5044
meryem <meryemsavas1234@gmail.com>
2024-06-10 10:26:16 +0200

    try

--- wp-content/themes/NiekHendrix/index.php
+++ wp-content/themes/NiekHendrix/index.php
@@ -1,10 +1,10 @@
-    
     <?php get_header() ?>
     <?php wp_nav_menu()  ?>
     <?php get_the_title() ?>
     <?php echo get_the_content() ?>
     <?php wp_footer() ?>
 
-  
-</body>
-</html>
+
+    </body>
+
+    </html>
