ba1bd1c0fe1f24355e9a26f3f6fea29b9c6d0183
Raghda <9015808@student.zadkine.nl>
2024-06-12 11:10:39 +0200

    deleted everything

--- wp-content/themes/NiekHendrix/index.php
+++
@@ -1,6 +0,0 @@
-    <?php get_header() ?>
-    <?php wp_nav_menu()  ?>
-    <?php get_the_title() ?>
-    <?php echo get_the_content() ?>
-    <?php wp_footer() ?>
-
--- wp-content/themes/NiekHendrix/page.php
+++
@@ -1,9 +0,0 @@
-<?php get_header() ?>
-<?php 
-while(have_posts()){
-    the_post();?>
-    <h1><?php the_title();?>
-    <div class="test"><?php the_content();?></div>
-    <?php
-}
-?>
